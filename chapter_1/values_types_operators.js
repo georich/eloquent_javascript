2baa9a17d82891e7029bae577b16c0c2f2308eb4
George Richards <g.richards101@gmail.com>
2018-03-04 22:24:52 +0000

    unary operators and boolean values

--- chapter_1/values_types_operators.js
+++ chapter_1/values_types_operators.js
@@ -4,8 +4,8 @@
 2.998e8 // exponent, 2.998 * 10^8
 
 // ARITHMETIC
-console.log(100 + 4 * 11)
-console.log(100 % 33) // modulo
+console.log(100 + 4 * 11);
+console.log(100 % 33); // modulo
 
 // SPECIAL NUMBERS
 Infinity
@@ -14,7 +14,24 @@ NaN // Not a number
 
 // STRINGS
 "A string would look like this"
-console.log("This is the first line\nAnd this is the second") // escape character as usual
+console.log("This is the first line\nAnd this is the second"); // escape character as usual
 "con" + "cat" + "e" + "nate" // "concatenate"
-console.log(`half of 100 is ${100 / 2}`)
+console.log(`half of 100 is ${100 / 2}`);
 // template literals (similar to f-string), requires the use of backticks `
+
+// UNARY OPERATORS
+console.log(typeof 4.5);
+console.log(typeof "x");
+
+// BOOLEAN VALUES
+// COMPARISON
+console.log(3 > 2); // true
+console.log("Apple" == "Orange"); // false
+console.log("Apple" != "Orange") // true
+console.log(NaN == NaN);
+// LOGICAL OPERATORS
+console.log(true && false); // false
+console.log(true && true); // true
+console.log(false || true) // true
+console.log(false || false) // false
+// CONDITIONAL OPERATOR
