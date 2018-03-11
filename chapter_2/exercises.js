288132717134309db3e49c75fa3607b621ee4d36
George Richards <g.richards101@gmail.com>
2018-03-11 22:02:01 +0000

    fizzbuzz solution

--- chapter_2/exercises.js
+++ chapter_2/exercises.js
@@ -1,5 +1,5 @@
+// LOOPING A TRIANGLE
 let triangleCount = 1;
-
 while (triangleCount < 8) {
   console.log("#".repeat(triangleCount));
   triangleCount++;
@@ -11,4 +11,17 @@ while (triangleCount < 8) {
   console.log(initalRow);
   initalRow = initalRow + "#";
   triangleCount++
+}
+
+// FIZZBUZZ
+for (let num = 1; num <= 100; num++) {
+  if (num % 15 == 0) {
+    console.log("FizzBuzz");
+  } else if (num % 3 == 0) {
+    console.log("Fizz");
+  } else if (num % 5 == 0) {
+    console.log("Buzz");
+  } else {
+    console.log(num)
+  }
 }
