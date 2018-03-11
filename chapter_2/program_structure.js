7b414ae0fcb829beee75251d7b36922533be49e3
George Richards <g.richards101@gmail.com>
2018-03-11 21:24:36 +0000

    control flow and conditional execution

--- chapter_2/program_structure.js
+++ chapter_2/program_structure.js
@@ -17,3 +17,27 @@ console.log(greeting + name);
 // CONSOLE.LOG FUNCTION
 let x = 30;
 console.log(`The value of x is ${x}`);
+
+// CONTROL FLOW
+let theNumber = Number(prompt("Pick a number")); // Number converts to number, equivalent of int()?
+console.log("Your number is the square root of " + theNumber * theNumber);
+
+// CONDITIONAL EXECUTION
+let theNumber = Number(prompt("Pick a number"));
+if (!isNaN(theNumber)) { // runs if isNaN evaluates to false
+  console.log("Your number is the square root of " + theNumber * theNumber);
+} else {
+  console.log("Hey. Why didn't you supply a number?");
+}
+
+if (1 + 1 == 2) console.log("It's true");
+
+let num = Number(prompt("Pick a number"));
+
+if (num < 10) {
+  console.log("Small number");
+} else if (num < 100) {
+  console.log("Medium number");
+} else {
+  console.log("Large number");
+}
