3202217db10dc939e983a3406880ca5d34b78ca9
George Richards <g.richards101@gmail.com>
2018-03-12 17:04:08 +0000

    methods

--- chapter_4/objects_and_arrays.js
+++ chapter_4/objects_and_arrays.js
@@ -4,3 +4,15 @@ console.log(listOfNumbers[2]); // 5
 
 // PROPERTIES
 listOfNumbers.length // length property of listOfNumbers
+
+// METHODS
+let doh = "Doh";
+console.log(typeof doh.toUpperCase); // function
+console.log(doh.toUpperCase()); // DOH
+
+let sequence = [1, 2, 3];
+sequence.push(4);
+sequence.push(5);
+console.log(sequence); // [1, 2, 3, 4, 5]
+console.log(sequence.pop()); // removes and returns last element, -> 5
+console.log(sequence); // [1, 2, 3, 4]
