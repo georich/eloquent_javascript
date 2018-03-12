ab0ffa7754df83854d1e74ed30f49ebbdbea0e7f
George Richards <g.richards101@gmail.com>
2018-03-12 18:24:55 +0000

    array loops

--- chapter_4/objects_and_arrays2.js
+++ chapter_4/objects_and_arrays2.js
@@ -14,3 +14,14 @@ addEntry(["work", "touched tree", "pizza", "running", "television"], false);
 addEntry(["work", "ice cream", "cauliflower", "lasagna", "touched tree", "brushed teeth"], false);
 addEntry(["weekend", "cycling", "break", "peanuts", "beer"], true);
 console.log(journal);
+
+// ARRAY LOOPS
+
+for (let i = 0; i < array.length; i++) { // common way to loop through all elements in an array
+  let entry = array[i];
+  // Do something
+}
+
+for (let entry of array) { // modern way of looping through elements, good for arrays, strings and some others
+  console.log(`${entry.events.length} events.`);
+}
