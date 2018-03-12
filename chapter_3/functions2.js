445c95010d342200a3d2675e76ac151750132a2b
George Richards <g.richards101@gmail.com>
2018-03-12 13:20:57 +0000

    closure

--- chapter_3/functions2.js
+++ chapter_3/functions2.js
@@ -20,3 +20,19 @@ function power(base, exponent = 2) { // default function paramater similar to py
 }
 console.log(power(4));
 console.log(power(2, 6));
+
+// CLOSURE
+function wrapValue(n) {
+  let local = n;
+  return () => local;
+}
+let wrap1 = wrapValue(1);
+let wrap2 = wrapValue(2);
+console.log(wrap1()); // 1
+console.log(wrap2()); // 2
+
+function multiplier(factor) {
+  return number => number * factor;
+}
+let twice = multiplier(2);
+console.log(twice(5)); // 10
