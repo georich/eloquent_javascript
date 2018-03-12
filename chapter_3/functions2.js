c0e655d56d6526889297b77e58d5c2dee0663c27
George Richards <g.richards101@gmail.com>
2018-03-12 13:57:02 +0000

    recursion and growing functions

--- chapter_3/functions2.js
+++ chapter_3/functions2.js
@@ -36,3 +36,73 @@ function multiplier(factor) {
 }
 let twice = multiplier(2);
 console.log(twice(5)); // 10
+
+// RECURSION
+// recursive solution for power()
+/*
+function power(base, exponent) {
+  if (exponent == 0) {
+    return 1;
+  } else {
+    return base * power(base, exponent - 1);
+  }
+}
+console.log(power(2, 3)); // 8
+*/
+
+// GROWING FUNCTIONS
+/*
+wanting to produce something like:
+007 Cows
+011 Chickens
+*/
+function printFarmInventory(cows, chickens) {
+  let cowString = String(cows);
+  while (cowString.length < 3) {
+    cowString = "0" + cowString;
+  }
+  console.log(`${cowString} Cows`);
+  let chickenString = String(chickens);
+  while (chickenString.length < 3) {
+    chickenString = "0" + chickenString;
+  }
+  console.log(`${chickenString} Chickens`);
+}
+printFarmInventory(7, 11);
+
+// and what if the farmer now has pigs too? can rewrite this into two functions
+// rather than adding the same while loop again to printFarmInventory
+
+function printZeroPaddedWithLabel(number, label) {
+  let numberString = String(number);
+  while (numberString.length < 3) {
+    numberString = "0" + numberString;
+  }
+  console.log(`${numberString} ${label}`);
+}
+
+function printFarmInventory(cows, chickens, pigs) {
+  printZeroPaddedWithLabel(cows, "Cows");
+  printZeroPaddedWithLabel(chickens, "Chickens");
+  printZeroPaddedWithLabel(pigs, "Pigs");
+}
+
+printFarmInventory(7, 11, 3);
+
+// but we can lift out a single concept to further make it more readable
+
+function zeroPad(number, width) {
+  let string = String(number);
+  while (string.length < width) {
+    string = "0" + string;
+  }
+  return string;
+}
+
+function printFarmInventory(cows, chickens, pigs) {
+  console.log(`${zeroPad(cows, 3)} Cows`);
+  console.log(`${zeroPad(chickens, 3)} Chickens`);
+  console.log(`${zeroPad(pigs, 3)} Pigs`);
+}
+
+printFarmInventory(7, 16, 3);
