155e4f537daa9dbeced620b97c12edd9f4c3fe0e
George Richards <g.richards101@gmail.com>
2018-03-12 12:41:24 +0000

    nested scope

--- chapter_3/functions.js
+++ chapter_3/functions.js
@@ -27,3 +27,21 @@ if (true) {
 }
 // y is not visible here
 console.log(x + z); // 40
+
+// NESTED SCOPE
+const hummus = function(factor) {
+  const ingredient = function(amount, unit, name) {
+    let ingredientAmount = amount * factor // not visible outside of this function
+    if (ingredientAmount > 1) {
+      unit += "s";
+    }
+    console.log(`${ingredientAmount} ${unit} ${name}`);
+  };
+  ingredient(1, "can", "chickpeas");
+  ingredient(0.25, "cup", "tahini");
+  ingredient(0.25, "cup", "lemon juice");
+  ingredient(1, "clove", "garlic");
+  ingredient(2, "tablespoon", "olive oil");
+  ingredient(0.5, "teaspoon", "cumin");
+};
+hummus(2);
