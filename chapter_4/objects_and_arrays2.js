270af5f9c1bd0d016954752fdf756080f085eda8
George Richards <g.richards101@gmail.com>
2018-03-12 19:49:30 +0000

    strings and their properties

--- chapter_4/objects_and_arrays2.js
+++ chapter_4/objects_and_arrays2.js
@@ -45,3 +45,24 @@ console.log([0, 1, 2, 3, 4].slice(2, 4)); // [2, 3], end index is exclusive
 console.log([0, 1, 2, 3, 4].slice(2)); // [2, 3, 4] slices to end if end point not given
 
 console.log([1, 2].concat([3, 4])); // [1, 2, 3, 4]
+
+// STRINGS AND THEIR PROPERTIES
+let kim = "Kim";
+kim.age = 88;
+console.log(kim.age); // undefined
+
+console.log("coconuts".slice(4, 7)); // nut
+console.log("coconut".indexOf("u")); // 5
+console.log("one two three".indexOf("ee")); // 11
+console.log("   okay  \n ".trim()); // "okay", trims all whitespace, newlines, tabs etc
+console.log(String(6).padStart(3, "0")); // 006
+
+let sentence = "Secretarybirds specialize in stomping";
+let words = sentence.split(" ");
+console.log(words.join(". ")); // Secretarybirds. specialize. in. stomping
+
+console.log("LA".repeat(3)); // LALALA
+
+let string = "abc";
+console.log(string.length); // 3
+console.log(string[1]); // b
