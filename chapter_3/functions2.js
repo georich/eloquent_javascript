c7b93f2db360aa31cd036f294f298c6365ea7373
George Richards <g.richards101@gmail.com>
2018-03-12 13:02:59 +0000

    optional arguments

---
+++ chapter_3/functions2.js
@@ -0,0 +1,22 @@
+// OPTIONAL ARGUMENTS
+function square(x) {
+  return x * x;
+}
+console.log(square(4, true, "hedgehog")); // ignores extra arguments and just computes using the first one
+
+function minus(a, b) {
+  if (b === undefined) return -a;
+  else return a - b;
+}
+console.log(minus(10)); // since b is left out undefined is passed instead
+console.log(minus(10, 5));
+
+function power(base, exponent = 2) { // default function paramater similar to python
+  let result = 1;
+  for (let count = 0; count < exponent; count++) {
+    result *= base;
+  }
+  return result;
+}
+console.log(power(4));
+console.log(power(2, 6));
