e0cc15ff3a095e1c1e4e54069ad219cfb81730c9
George Richards <g.richards101@gmail.com>
2018-03-13 15:16:11 +0000

    higher order functions

--- chapter_5/higher_order_functions.js
+++ chapter_5/higher_order_functions.js
@@ -29,3 +29,33 @@ repeat(5, i => {
   labels.push(`Unit ${i + 1}`);
 });
 console.log(labels);
+
+// HIGHER ORDER FUNCTIONS
+// create new functions
+function greaterThan(n) {
+  return m => m > n;
+}
+let greaterThan10 = greaterThan(10);
+console.log(greaterThan10(11)); // true
+// change other functions
+function noisy(f) {
+  return (...args) => {
+    console.log("calling with", args);
+    let result = f(...args);
+    console.log("called with", args, ", returned", result);
+    return result;
+  };
+}
+noisy(Math.min)(3, 2, 1);
+// write functions that provide new types of control flow
+function unless(test, then) {
+  if (!test) then();
+}
+repeat(3, n => {
+  unless(n % 2 == 1, () => {
+    console.log(n, "is even");
+  });
+});
+// built in array method that provides something like a for/of loop
+// as a higher function
+["A", "B"].forEach(l => console.log(l));
