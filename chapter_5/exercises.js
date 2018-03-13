2587bd321f4e333025ffe06c4442b9790499fb01
George Richards <g.richards101@gmail.com>
2018-03-13 22:44:21 +0000

    writing a custom for loop function

--- chapter_5/exercises.js
+++ chapter_5/exercises.js
@@ -1,3 +1,11 @@
 // FLATTENING
 let arrays = [[1, 2, 3], [4, 5], [6]];
 console.log(arrays.reduce((a, b) => a.concat(b)));
+
+// YOUR OWN LOOP
+function loop(value, limit, step, fn) {
+  for (let i = value; limit(i); i = step(i)) {
+    fn(i);
+  }
+}
+loop(3, n => n > 0, n => n - 1, console.log);
