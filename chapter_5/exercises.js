094397c3c6786a437fceb98d181c3258e4cc2f04
George Richards <g.richards101@gmail.com>
2018-03-13 23:03:54 +0000

    creating two manual every methods using loops and then the some method

--- chapter_5/exercises.js
+++ chapter_5/exercises.js
@@ -9,3 +9,26 @@ function loop(value, limit, step, fn) {
   }
 }
 loop(3, n => n > 0, n => n - 1, console.log);
+
+// EVERYTHING
+function every(array, test) {
+  for (let element of array) {
+    if (!test(element)) {
+      return false;
+    }
+  }
+  return true;
+}
+console.log(every([1, 3 ,5], n => n < 10));
+console.log(every([2, 4, 16], n => n < 10));
+console.log(every([], n => n < 10));
+
+function everySome(array, test) {
+  if (array.some(element => !test(element))) {
+    return false;
+  }
+  return true;
+}
+console.log(everySome([1, 3, 5], n => n < 10));
+console.log(everySome([2, 4, 16], n => n < 10));
+console.log(everySome([], n => n < 10));
