4f382264ad21befa59f2c95024831939344026fb
George Richards <g.richards101@gmail.com>
2018-03-13 19:33:01 +0000

    strings and character codes

--- chapter_5/higher_order_functions2.js
+++ chapter_5/higher_order_functions2.js
@@ -16,3 +16,24 @@ for (let script of SCRIPTS) {
 }
 console.log(Math.round(total / count));
 // filter and map method is slower but more readable and can have intermediate steps extracted out
+
+// STRINGS AND CHARACTER CODES
+function characterScript(code) {
+  for (let script of SCRIPTS) {
+    if (script.ranges.some(([from, to]) => {
+      return code >= from && code < to;
+    })) {
+      return script;
+    }
+  }
+  return null;
+}
+console.log(characterScript(121));
+// some method is a higher-order function
+// it takes a test function and tells you if that function returns true for any 
+// of the elements in the array
+let horseShoe = "🐴👟";
+console.log(horseShoe.codePointAt(0)); // will give actual code for an emoji since it is two units
+for (let char of horseShoe) { // for/of is useful for iterating over
+  console.log(char);
+}
