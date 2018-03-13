67cd8c2b767cacbcd8e8c39203e38c6e65529e8a
George Richards <g.richards101@gmail.com>
2018-03-13 19:42:56 +0000

    recognising text

--- chapter_5/higher_order_functions2.js
+++ chapter_5/higher_order_functions2.js
@@ -37,3 +37,35 @@ console.log(horseShoe.codePointAt(0)); // will give actual code for an emoji sin
 for (let char of horseShoe) { // for/of is useful for iterating over
   console.log(char);
 }
+
+// RECOGNISING TEXT
+// a method to count characters belonging to a script
+function countBy(items, groupName) {
+  let counts = [];
+  for (let item of items) {
+    let name = groupName(item);
+    let known = counts.findIndex(c => c.name == name); // finds first value where function returns true, returns -1 if it cannot
+    if (known == -1) {
+      counts.push({name, count: 1});
+    } else {
+      counts[known].count++;
+    }
+  }
+  return counts;
+}
+console.log(countBy([1, 2, 3, 4, 5], n => n > 2));
+// can now identify scripts in text
+function textScripts(text) {
+  let scripts = countBy(text, char => {
+    let script = characterScript(char.codePointAt(0));
+    return script ? script.name : "none";
+  }).filter(({name}) => name != "none");
+
+  let total = scripts.reduce((n, {count}) => n + count, 0);
+  if (total == 0) return "No scripts found";
+
+  return scripts.map(({name, count}) => {
+    return `${Math.round(count * 100 / total)}% ${name}`;
+  }).join(", ");
+}
+console.log(textScripts('英国的狗说"woof", 俄罗斯的狗说"тяв"'));
