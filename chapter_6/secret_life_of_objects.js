856f175a6dd72927d5d252ebc56fdb5cf999f227
George Richards <g.richards101@gmail.com>
2018-03-14 11:12:07 +0000

    prototypes

--- chapter_6/secret_life_of_objects.js
+++ chapter_6/secret_life_of_objects.js
@@ -26,3 +26,32 @@ function normalize() {
   console.log(this.coords.map(n => n / this.length));
 }
 normalize.call({coords: [0, 2, 3], length: 5});
+
+// PROTOTYPES
+let empty = {};
+console.log(empty.toString);
+console.log(empty.toString());
+// a property was pulled out of this empty object because of prototypes. When an object
+// gets a request for a property it doesn't have, it defaults to it's prototype.
+// For an empty object this is the Object.protoype
+console.log(Object.getPrototypeOf({}) == Object.prototype); // true
+console.log(Object.getPrototypeOf(Object.prototype)); // null
+// as we can see an empty object returns true, though acting on object.prototype it returns
+// false because it is the highest branch. Object.prototype contains a few methods such as toString
+// Not all objects derive from Object.prototype, Functions derive from Function.prototype, arrays
+// derive from Array.prototype.
+console.log(Object.getPrototypeOf(Math.max) == Function.prototype); // true
+console.log(Object.getPrototypeOf([]) == Array.prototype); // true
+// can use object.create to create an object with a specific prototype
+let protoRabbit = {
+  speak(line) {
+    console.log(`The ${this.type} rabbit says '${line}'`);
+  }
+};
+let killerRabbit = Object.create(protoRabbit);
+killerRabbit.type = "killer";
+killerRabbit.speak("SKREEE!");
+// property like speak(line) is shorthand for defining a method, creates a property speak and gives
+// it a function as a value
+// proto rabbit acts as a container for properties shared by all rabbits. Killer rabbit contains
+// properties applying only to itself
