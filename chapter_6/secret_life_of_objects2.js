056fbf659ec0de5993448a1b11abd5f1b589041f
George Richards <g.richards101@gmail.com>
2018-03-14 23:00:34 +0000

    getters, setters and statics

--- chapter_6/secret_life_of_objects2.js
+++ chapter_6/secret_life_of_objects2.js
@@ -133,3 +133,41 @@ let matrix = new Matrix(2, 2, (x, y) => `value ${x},${y}`);
 for (let {x, y, value} of matrix) {
   console.log(x, y, value);
 }
+
+// GETTERS, SETTERS AND STATICS
+let varyingSize = {
+  get size() {
+    return Math.floor(Math.random() * 100);
+  }
+};
+console.log(varyingSize.size);
+console.log(varyingSize.size);
+
+class Temperature {
+  constructor(celsius) {
+    this.celsius = celsius;
+  }
+
+  get fahrenheit() {
+    return this.celsius * 1.8 + 32;
+  }
+
+  set fahrenheit(value) {
+    this.celsius = (value - 32) / 1.8;
+  }
+
+  static fromFahrenheit(value) {
+    return new Temperature((value - 32) / 1.8);
+  }
+}
+
+let temp = new Temperature(22);
+console.log(temp.fahrenheit); // 71.6
+temp.fahrenheit = 86;
+console.log(temp.celsius); // 30
+// allows you to read and write temp in either Celsius or
+// Fahrenheit, but only stores it in Celsius, and automatically
+// converts to Celsius in the fahrenheit getter and setter.
+// Methods written with static are stored in the constructor,
+// so Temperature class allows you to write Temperature.fromFahrenheit(100)
+// to create a temperature using Fahrenheit
