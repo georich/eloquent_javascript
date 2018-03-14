c97899a36b2bea65b607c13f1b2c7af7c7638843
George Richards <g.richards101@gmail.com>
2018-03-14 23:13:36 +0000

    instanceof operator

--- chapter_6/secret_life_of_objects2.js
+++ chapter_6/secret_life_of_objects2.js
@@ -193,3 +193,9 @@ class SymmetricMatrix extends Matrix {
 }
 let matrixTwo = new SymmetricMatrix(5, (x, y) => `${x},${y}`);
 console.log(matrixTwo.get(2, 3)); // 3, 2
+
+// INSTANCEOF OPERATOR
+console.log(new SymmetricMatrix(2) instanceof SymmetricMatrix); // true
+console.log(new SymmetricMatrix(2) instanceof Matrix); // true
+console.log(new Matrix(2, 2) instanceof SymmetricMatrix); // false
+console.log([1] instanceof Array); // true
