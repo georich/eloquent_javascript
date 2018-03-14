36fd1f43e03db9c8b3c7c5462187b5984eb247a9
George Richards <g.richards101@gmail.com>
2018-03-14 21:36:54 +0000

    symbols

--- chapter_6/secret_life_of_objects2.js
+++ chapter_6/secret_life_of_objects2.js
@@ -46,3 +46,23 @@ Rabbit.prototype.toString = function() {
 };
 console.log(String(blackRabbit)); // a black rabbit
 // different kinds of objects can be plugged into toString, this is polymorphism
+
+// SYMBOLS
+// unlike strings, newly created symbols are unique, you cannot create the same symbol twice
+let sym = Symbol("name");
+console.log(sym == Symbol("name")); // false
+Rabbit.prototype[sym] = 55;
+console.log(blackRabbit[sym]); // 55
+// symbols are good for defining interfaces to live alongside other propertioes
+// no matter the name
+const toStringSymbol = Symbol("toString");
+Array.prototype[toStringSymbol] = function() {
+  return `${this.length} cm of blue yarn`;
+};
+console.log([1, 2].toString()); // 1, 2
+console.log([1, 2][toStringSymbol]()); // 2cm of blue yarn
+// can include symbol properties by using square brackets
+let stringObject = {
+  [toStringSymbol]() { return "a jute rope"; }
+};
+console.log(stringObject[toStringSymbol]()); // a jute rope
