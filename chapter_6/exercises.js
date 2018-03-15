a5dba5c2e93f85b712e953f55af3c5af4bd4d557
George Richards <g.richards101@gmail.com>
2018-03-15 17:13:30 +0000

    groups exercise solution

--- chapter_6/exercises.js
+++ chapter_6/exercises.js
@@ -20,3 +20,46 @@ class Vec {
 console.log(new Vec(1, 2).plus(new Vec(2, 3)));
 console.log(new Vec(1, 2).minus(new Vec(2, 3)));
 console.log(new Vec(3, 4).length);
+
+// GROUPS
+class Group {
+  constructor() {
+    this.group = [];
+  }
+
+  add(value) {
+    if (!this.has(value)) {
+      this.group.push(value);
+    }
+  }
+  
+  delete(value) {
+    // let tempArray = [];
+    if (this.has(value)) {
+      this.group = this.group.filter(i => i != value);
+      // console.log(tempArray);
+      // this.group = tempArray;
+    }
+  }
+
+  has(value) {
+    return this.group.includes(value);
+  }
+  
+  static from(iterable) {
+    let group = new Group;
+    for (let value of iterable) {
+      group.add(value);
+    }
+    return group;
+  }
+}
+
+let group = Group.from([0, 5, 10])
+group.add(10);
+group.add(10);
+group.add(15);
+console.log(group.group);
+console.log(group.has(10));
+group.delete(10);
+console.log(group.group);
