8b8ae0e96be76fb9a86c5ab6adac3e9697435b17
George Richards <g.richards101@gmail.com>
2018-03-17 14:09:55 +0000

    the task understanding

--- chapter_7/robot.js
+++ chapter_7/robot.js
@@ -28,3 +28,34 @@ function buildGraph(edges) {
 }
 // console.log(buildGraph(roads));
 const roadGraph = buildGraph(roads);
+
+// THE TASK
+class VillageState {
+  constructor(place, parcels) {
+    this.place = place;
+    this.parcels = parcels;
+  }
+
+  move(destination) {
+    if (!roadGraph[this.place].includes(destination)) {
+      return this;
+    } else {
+      let parcels = this.parcels.map(p => {
+        if (p.place != this.place) {
+          return p;
+        }
+        return {place: destination, address: p.address};
+      }).filter(p => p.place != p.address);
+      return new VillageState(destination, parcels);
+    }
+  }
+}
+
+let first = new VillageState(
+  "Post Office",
+  [{place: "Post Office", address: "Alice's House"}]
+);
+let next = first.move("Alice's House");
+console.log(next.place);
+console.log(next.parcels);
+console.log(first.place);
