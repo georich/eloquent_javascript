6d701ac98e6b60e09cb3286f368cd0217c6dfd40
George Richards <g.richards101@gmail.com>
2018-03-17 13:57:34 +0000

    setting the graph of roads/destinations for the robot

---
+++ chapter_7/robot.js
@@ -0,0 +1,30 @@
+// MEADOWFIELD
+const roads = [
+  "Alice's House-Bob's House",   "Alice's House-Cabin",
+  "Alice's House-Post Office",   "Bob's House-Town Hall",
+  "Daria's House-Ernie's House", "Daria's House-Town Hall",
+  "Ernie's House-Grete's House", "Grete's House-Farm",
+  "Grete's House-Shop",          "Marketplace-Farm",
+  "Marketplace-Post Office",     "Marketplace-Shop",
+  "Marketplace-Town Hall",       "Shop-Town Hall"
+];
+// we are interested in the destinations that can be reached from one point
+// will need to convert the list into a data structure that tells us what 
+// can be reached from where
+function buildGraph(edges) {
+  let graph = Object.create(null);
+  function addEdge(from, to) {
+    if (graph[from] == null) {
+      graph[from] = [to]; // if key doesn't exist then add it and the destination
+    } else {
+      graph[from].push(to); // if it did then just add the destination
+    }
+  }
+  for (let [from, to] of edges.map(r => r.split("-"))) {
+    addEdge(from, to);
+    addEdge(to, from);
+  }
+  return graph;
+}
+// console.log(buildGraph(roads));
+const roadGraph = buildGraph(roads);
