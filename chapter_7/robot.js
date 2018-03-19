183ce55a131daa93a7dab3f16e6b31c08eb5939c
George Richards <g.richards101@gmail.com>
2018-03-19 18:38:23 +0000

    adding an optimal route finder for the robots path

--- chapter_7/robot.js
+++ chapter_7/robot.js
@@ -125,3 +125,34 @@ function routeRobot(state, memory) {
 }
 runRobot(VillageState.random(), routeRobot, []);
 // now finishing in 20 turns
+
+// PATHFINDING
+// need to find shortest route
+function findRoute(graph, from, to) {
+  let work = [{at: from, route: []}];
+  for (let i = 0; i < work.length; i++) {
+    let {at, route} = work[i];
+    for (let place of graph[at]) {
+      if (place == to) {
+        return route.concat(place);
+      }
+      if (!work.some(w => w.at == place)) {
+        work.push({at: place, route: route.concat(place)});
+      }
+    }
+  }
+}
+
+function goalOrientedRobot({place, parcels}, route) {
+  if (route.length == 0) {
+    let parcel = parcels[0];
+    if (parcel.place != place) {
+      route = findRoute(roadGraph, place, parcel.place);
+    } else {
+      route = findRoute(roadGraph, place, parcel.address);
+    }
+  }
+  return {direction: route[0], memory: route.slice(1)};
+}
+// now finishes in an even shorter time since optimal route is ccalculated
+runRobot(VillageState.random(), goalOrientedRobot, []);
