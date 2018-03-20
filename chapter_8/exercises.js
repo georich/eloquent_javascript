9574b52eeb2614236e5d0ae508e4f7aeb7417f11
George Richards <g.richards101@gmail.com>
2018-03-20 17:00:41 +0000

    locked box exercise solution

--- chapter_8/exercises.js
+++ chapter_8/exercises.js
@@ -27,3 +27,42 @@ function reliableMultiply(a, b) {
   return result;
 }
 console.log(reliableMultiply(8, 8));
+
+// THE LOCKED BOX
+const box = {
+  locked: true,
+  unlock() { this.locked = false; },
+  lock() { this.locked = true; },
+  _content: [],
+  get content() {
+    if (this.locked) throw new Error("Locked!");
+    return this._content;
+  }
+};
+
+function withBoxUnlocked(body) {
+  let locked = box.locked;
+  if (!locked) {
+    return body();
+  }
+  box.unlock();
+  try {
+    return body();
+  } finally {
+    box.lock();
+  }
+}
+
+withBoxUnlocked(function() {
+  box.content.push("gold piece");
+});
+
+try {
+  withBoxUnlocked(function() {
+    throw new Error("Pirates on the horizon! Abort!");
+  });
+} catch (e) {
+  console.log("Error raised:", e);
+}
+
+console.log(box.locked);
