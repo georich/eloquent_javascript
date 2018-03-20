9097ea5fa8c13aa869ee5db4af8dd990ab510abc
George Richards <g.richards101@gmail.com>
2018-03-20 15:25:42 +0000

    error propagation

--- chapter_8/bugs_and_errors.js
+++ chapter_8/bugs_and_errors.js
@@ -64,3 +64,23 @@ function numberToString(n, base = 10) {
   return sign + result;
 }
 console.log(numberToString(13, 10));
+
+// ERROR PROPAGATION
+function promptNumber(question) {
+  let result = Number(prompt(question));
+  if (isNaN(result)) {
+    return null;
+  } else {
+    return result;
+  }
+}
+console.log(promptNumber("How many trees do you see?"));
+
+// what if the function can return every kind of value?
+function lastElement(array) {
+  if (array.length == 0) {
+    return {failed: true};
+  } else {
+    return {element: array[array.length - 1]};
+  }
+}
