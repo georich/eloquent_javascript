b5b2588904b124ae1835aa8a8e6df0c14438d21f
George Richards <g.richards101@gmail.com>
2018-03-21 20:02:03 +0000

    quoting style change using regexp

--- chapter_9/exercises.js
+++ chapter_9/exercises.js
@@ -42,3 +42,7 @@ function verify(regexp, yes, no) {
     }
   }
 }
+
+// QUOTING STYLE
+let text = "'I'm the cook,' he said, 'it's my job.'";
+console.log(text.replace(/(^|\W)'|'(\W|$)/g, '$1"$2'));
