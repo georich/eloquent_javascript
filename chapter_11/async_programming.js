164bddb66f9d24a6aa484dad6bfa3da9cabe0635
George Richards <g.richards101@gmail.com>
2018-03-26 23:44:48 +0100

    network flooding

--- chapter_11/async_programming.js
+++ chapter_11/async_programming.js
@@ -131,3 +131,26 @@ function availableNeighbors(nest) {
     return nest.neighbors.filter((_, i) => result[i]);
   });
 }
+
+// NETWORK FLOODING
+const {everywhere} = require("./crow-tech");
+
+everywhere(nest => {
+  nest.state.gossip = [];
+});
+
+function sendGossip(nest, message, exceptFor = null) {
+  nest.state.gossip.push(message);
+  for (let neighbor of nest.neighbors) {
+    if (neighbor == exceptFor) continue;
+    request(nest, neighbor, "gossip", message);
+  }
+}
+
+requestType("gossip", (nest, message, source) => {
+  if (nest.state.gossip.includes(message)) return;
+  console.log(`${nest.name} received gossip '${message}' from ${source}`);
+  sendGossip(nest, message, source);
+});
+
+sendGossip(bigOak, "Kids with airgun in the park");
