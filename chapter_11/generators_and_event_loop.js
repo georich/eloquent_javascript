ccc72f70ba882a971495b02addce327c7c96365e
George Richards <g.richards101@gmail.com>
2018-03-27 15:28:09 +0100

    generators understanding

---
+++ chapter_11/generators_and_event_loop.js
@@ -0,0 +1,11 @@
+// GENERATORS
+function* powers(n) { // asterik after function declaration declares a generator
+  for (let current = n;; current *= n) {
+    yield current;
+  }
+}
+
+for (let power of powers(3)) {
+  if (power > 50) break;
+  console.log(power);
+}
